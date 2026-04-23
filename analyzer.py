ee89991343445025e8ccab6ceb62988bff2bfb0c
Adane Vincent <adanevincent13@gmail.com>
2026-04-23 18:55:19 +0000

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -2,3 +2,11 @@ with open("sample.log") as file:
     for line in file:
         if "Failed password" in line:
             print(line)
+
+count=0
+with open("sample.log") as file:
+    for line in file:
+        if "Failed password" in line:
+            print(line.strip())
+            count=count+1
+print("Total failed attempts:",count)        
